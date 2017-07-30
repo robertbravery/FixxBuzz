e9f89b4b134980145d566f932e21a87754ef8a26
Robert Bravery <rbravery@iqbusiness.net>
2017-07-30 18:50:14 +0200

    CHanges to test Bamboo repository detection

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,7 @@ namespace FizzBuzz
 {
     class Program
     {
-        static List<double> forLoop = new List<double>();
+        static readonly List<double> ForLoop = new List<double>();
         static List<double> forLoop1 = new List<double>();
         static List<double> linqLoop = new List<double>();
             
@@ -24,14 +24,14 @@ namespace FizzBuzz
                 LinqLoop();
             }
             Console.WriteLine($"Loop 1 average time: {forLoop1.Average()} ms, Total time {forLoop1.Sum()/1000} s");
-            Console.WriteLine($"Avery time for loop: {forLoop.Average()} ms, Total time {forLoop.Sum() / 1000} s");
+            Console.WriteLine($"Avery time for loop: {ForLoop.Average()} ms, Total time {ForLoop.Sum() / 1000} s");
             Console.WriteLine($"Avery time for linq: {linqLoop.Average()} ms, Total time {linqLoop.Sum() / 1000} s");
             Console.ReadLine();
         }
 
         private static void ForLoop1()
         {
-            var duration = ForLoop.Loop(1000);
+            var duration = FizzBuzz.ForLoop.Loop(1000);
             if (duration > 0) Console.WriteLine($"Time taken for normal 'for Loop': {duration}");
             forLoop1.Add(duration);
         }
@@ -40,7 +40,7 @@ namespace FizzBuzz
         {
             var duration = FizzBuzz.NormalLoop.Loop(1000);
             if (duration > 0) Console.WriteLine($"Time taken for normal Loop: {duration}");
-            forLoop.Add(duration);
+            ForLoop.Add(duration);
         }
 
         private static void LinqLoop()
